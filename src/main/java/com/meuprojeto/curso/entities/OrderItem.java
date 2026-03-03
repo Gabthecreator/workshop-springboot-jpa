150b0189bb94d9ef6e6dc9ea2909da01b1af4559
Gabriel Victor <gabriielufrn14@gmail.com>
2026-03-03 17:08:40 -0300

    Exception handling - findById

--- src/main/java/com/meuprojeto/curso/entities/OrderItem.java
+++ src/main/java/com/meuprojeto/curso/entities/OrderItem.java
@@ -13,6 +13,8 @@ import java.util.Objects;
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @EmbeddedId
     private OrderItemPK id = new OrderItemPK();
 
--- src/main/java/com/meuprojeto/curso/service/UserService.java
+++ src/main/java/com/meuprojeto/curso/service/UserService.java
@@ -2,6 +2,7 @@ package com.meuprojeto.curso.service;
 
 import com.meuprojeto.curso.entities.User;
 import com.meuprojeto.curso.repositories.UserRepository;
+import com.meuprojeto.curso.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
